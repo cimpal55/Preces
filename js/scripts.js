87bc8d24330196fde9eb4edb574dab0066f3dc3f
cimpal55 <cimpals71@gmail.com>
2021-04-20 17:00:05 +0300

    3 izmaina

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const formdiv = document.getElementById('pop');
+
+document.getElementById('addprec').addEventListener('click', () => {
+    formdiv.style.display = 'block';
+})
+
+document.getElementById('pievienot').addEventListener('click', () => {
+    formdiv.style.display = 'none';
+})
